745c6a9d46181e6745c2e9fe7c78de2668bd9f8a
Nourhan <nourhanelyamany@gmail.com>
2021-07-06 08:29:08 +0200

    class writeFile

--- Write Json on Disk/jsonFile.py
+++ Write Json on Disk/jsonFile.py
@@ -5,11 +5,13 @@ from PIL import Image
 submitables = {
     "test":"yes", "test":"no"
 }
+def writeFile(submitables) :
 
-jsonString = json.dumps(submitables)
-print(jsonString)
-jsonFile = open("data.json", "w")
-jsonFile.write(jsonString)
-jsonFile.close()
-img = Image.open("QR_code/test.png",'r')
-img.save('data.png', 'PNG')
+    jsonString = json.dumps(submitables)
+    jsonFile = open("data.json", "w")
+    jsonFile.write(jsonString)
+    jsonFile.close()
+
+    
+    img = Image.open("QR_code/test.png",'r')
+    img.save('data.png', 'PNG')
