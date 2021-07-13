8a3b20cc1882f9f8c0bd9fe61eba35976a11be56
Omar Tamer <omar85zaki@gmail.com>
2021-07-13 10:36:00 +0200

    fixing import isssue of getAlphaNumeric

--- AlphanumericCharacterDetection/modelApproach/alphanum_B.py
+++ AlphanumericCharacterDetection/modelApproach/alphanum_B.py
@@ -42,8 +42,7 @@ from recogniser import Recognize
 from os import remove
 import time
 import numpy as np
-from PIL import Image
-# from getAlphaNumeric import getAlphaNumeric
+import getAlphaNumeric
 
 WHITE_LIST = ['A','B','C','c','D','E','F','G','H','I','J','K','k','L','l','M','m','N','O','o','P','p','Q','R','S','s','T','U','u','V','v','W','w','X','x','Y','y','Z','z','0','1','2','3','4','5','6','7','8','9']
 
@@ -60,38 +59,11 @@ def alphanum_B(image, id):
 	return out_character
 
 
-def getAlphaNumeric(imagefile):
-	
-	# crop image
-	height =imagefile.shape[0]
-	width =imagefile.shape[1]
-	height_to_crop = int(height * (20/100))
-	width_to_crop = int(width * (20/100))
-	x_border = height -(height_to_crop*2)
-	y_border = width -( width_to_crop*2)
-	cropped_image = imagefile[ height_to_crop : height_to_crop+ (x_border),
-	                          width_to_crop: width_to_crop+ ( y_border) ]
-
-
-	
-	lower_white = np.array([0,0,0], dtype=np.uint8)
-	upper_white = np.array([200,100,255], dtype=np.uint8)
-
-	mask = cv2.inRange(cropped_image,lower_white,upper_white)
-
-
-	cv2.imshow("Hey",image)
-	cv2.imshow("crop",cropped_image)
-	cv2.imshow("mask",mask)
-	
-	cv2.waitKey(0)
-	return alphanum_B(mask, 1)
 
 if __name__ == '__main__':
-	image  = cv2.imread("Sample4.jpg")
-	
-	
+	image  = cv2.imread("Sample10.jpg")
+
 	timer = time.perf_counter()
-	character = getAlphaNumeric(image)
+	character = getAlphaNumeric.getAlphaNumeric(image)
 	print(time.perf_counter()-timer)
 	print(character)
