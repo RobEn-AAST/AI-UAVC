6f798fdb88487bb2764a38704401267989fbd57f
Mohameosama <moandosama1163@gmail.com>
2021-07-30 21:53:27 +0200

    on working

--- geotag/geo.py
+++ geotag/geo.py
@@ -6,13 +6,17 @@ import json
 def repeatedTarget(loc):
    path = (os.path.dirname(os.path.abspath(__file__)))
    with open('geotags.json','r') as f:
+      geos = json.load(f)
       Location = str(loc)
       Location = Location[1:-1]
       lat, lon = Location.split(',')
       lat = float(lat)
       lon = float(lon)
-      geos = json.load(f)
-
+      if len(geos) == 0:
+          with open('geotags.json','w') as f:
+               geos["geos"].append({"lat":lat, "lon":lon})
+               json.dump(geos,f)
+         return False            
       for i in range(len(geos)):
          result = ((((lat - geos["geos"][i]["lat"] )**2) + ((lon - geos["geos"][i]["lon"])**2) )**0.5)
          if result>=30: # calculate distance avbout 20 metetr from the geolocation
