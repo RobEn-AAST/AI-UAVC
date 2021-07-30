f3d36c044d778c775b7be6028e6abbadac0a0f9d
karimkohel <kareemkohel@gmail.com>
2021-07-30 13:20:02 +0200

    main refactor

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-"""
-This is the base plate code for the main file
-    - This code can and will change according to the need of each function developed
-    - Naming and base logic are subject to change
-"""
-
-# import any needed libs like so
-from QR_code.QR_function import checkQR
 from WriteJsonOnDisk.jsonFile import submitToUSB
 from geotag.geo import repeatedTarget
-from AlphanumericCharacterDetection.getAlphaNumeric import getAlphaNumeric
+import AlphanumericCharacterDetection.AlphaNumeric
 from dataTransimission.server_station.UAV_SERVER import UAV_SERVER
 
 server = UAV_SERVER()
@@ -16,32 +8,23 @@ mission = {}
 detectedCount = 0
 terminate = True
 
-
 while terminate:
-    # every {0.5} sec we will =>
-    terminate, location, img = server.receiveMissions() # Receive image and geotag(long, lat) from UAV 
-    qrPresent, value = checkQR(img) # check if there is a QR code in the image and return value if so
-    mission["latitude"], mission["longitude"] = location # osama to get long and lat lowa7dohom
+    terminate, location, img = server.receiveMissions()
+    mission["latitude"], mission["longitude"] = location # osama to get long and lat lowa7dohom TODO: get lat and long alone
 
-    if qrPresent:
-        mission["QRimg"] = img
-        mission["type"] = "QR-code"
-        mission["value"] = value
-    else:
-        result, boundRect, found = detectShape(img) # AI returns ("Enemy", (lat, long), true) (crops image to bounding rect)
-        if found and (not repeatedTarget(boundRect)):
-            detectedCount = detectedCount + 1
-            cropedTarget = cropToRect(img)
-            mission["type"] = result # enemy or allie (as string)
-            mission["alphanumeric"] = getAlphaNumeric(cropedTarget) # alphanumeric detection
+    result, boundRect, found = detectShape(img) # AI returns ("Foe", (lat, long), true) (crops image to bounding rect) TODO: wrap the AI 
 
-            # place values in dict (could be cleaner but leave it for now)
-            mission["img"] = cropedTarget
-            mission["originalImage"] = img
+    if found and (not repeatedTarget(location)): # TODO: finish repeated target if found a repeated location tag
+        detectedCount = detectedCount + 1
+        croppedTarget = cropToRect(img) # TODO: crop to rect or get it cropped from detect shape 
+        mission["type"] = result
+        mission["alphanumeric"] = AlphanumericCharacterDetection.AlphaNumeric.getAlphaNumeric(croppedTarget)
 
-            jsonLocation = submitToUSB(mission, detectedCount) # Serialization
-            submitToJudge(jsonLocation) # DevOps
-            # count for the times of running the function , increment the count
+        mission["img"] = croppedTarget
+        mission["originalImage"] = img
 
-            if result == "Friend": # RR = Red Rectangel
-                sendUAV(location) # MavLink
+        jsonLocation = submitToUSB(mission, detectedCount)
+        submitToJudge(jsonLocation) # DevOps TODO: finish interop wrapping
+
+        if result == "Friend": # RR = Red Rectangel
+            sendUAV(location) # MavLink TODO: see Emad/salma/abbas about this unholy function
