bc4e7e98163c381699335e07f26bdd765a2e7f85
karimkohel <kareemkohel@gmail.com>
2021-08-02 23:14:03 +0200

    writeToUsb refactor

--- WriteJsonOnDisk/jsonFile.py
+++ WriteJsonOnDisk/jsonFile.py
@@ -1,30 +1,14 @@
-# import module
 import json
-from PIL import Image
 import cv2
 
-# img = cv2.imread("/test.pngQR_code")
 
-# c=1
-# submitablesImg = {
-#     "img": img, "alphanumeric": "A" , "geotag" : "xxx" , "color" : "red"
-# }
-# submitablesQR = {
-#     "QRimg": img, "QRvalue": "xxxxx", "geotag" : "xxxx"
-# }
-
-# TODO
-#   - change path to relative depending on this file locatoins
-#   - return the path of each deliverable made with it's name
-#
-
-def submitToUSB(submitables,c) :
+def submitToUSB(submitables, img, c) :
 
     jsonString = json.dumps(submitables)
-    jsonFile = open(f"WriteJsonOnDisk/Image/{c}.json", "w")
-    jsonFile.write(jsonString)
-    jsonFile.close()
 
-    img = Image.open("QR_code/test.png",'r')
-    img.save(f'WriteJsonOnDisk/Image/{c}.png', 'PNG')
+    with open(f"submitables/{c}.json", "w") as f:
+        f.write(jsonString)
 
+    cv2.imwrite(f"submitables/{c}.png", img)
+
+    return f"submitables/{c}.png"
