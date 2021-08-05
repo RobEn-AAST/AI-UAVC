0c5149099d5c3095760035c5b6ab251447678345
mhwahdan <moustafawahdan55@gmail.com>
2021-08-05 18:22:35 +0200

    fixed the pi-client + added logging and monitoring

--- dataTransimission/Client_PI/UAV_CLIENT.py
+++ dataTransimission/Client_PI/UAV_CLIENT.py
@@ -6,9 +6,9 @@ import pickle
 import struct
 from threading import Thread, Lock
 import numpy as np
-from dronekit import connect
-import pymavlink
-from pymavlink import mavutil
+from pymavlink.mavutil import mavlink_connection
+import logging
+from datetime import datetime
 
 ConnectionThreadLock = Lock()
 class ConnectionThread(Thread): #A thread for maintaning a redundant connection
@@ -19,6 +19,7 @@ class ConnectionThread(Thread): #A thread for maintaning a redundant connection
     def run(self):
         ConnectionThreadLock.acquire()
         self.socket.__init__(ADDRESS = self.socket.ADDRESS)
+        logging.WARNING("Connection regained")
         ConnectionThreadLock.release()
 
 
@@ -48,7 +49,7 @@ class UAV_CLIENT(socket):
             self.PORT = PORT
             self.settimeout(2)
         except ConnectionRefusedError as e:
-            print("failed to establish connection due to : " + str(e))
+            logging.WARNING("Failed to establish connection due to : " + str(e))
             self.initialized = False
 
     def sendMission(self, geolocation = None, image = None, Finished= False):
@@ -62,10 +63,7 @@ class UAV_CLIENT(socket):
                     "finished" : Finished
                 }
                 self.Queue.append(mission)
-            try:
                 out = self.Queue.pop(0)
-            except:
-                return True
             if not self.initialized:
                 if not (geolocation == None and image == None):
                     self.Queue.append(mission)
@@ -75,14 +73,13 @@ class UAV_CLIENT(socket):
             self.sendall(struct.pack(">L", SegmentsNumber) + Segments)
             response = self.recv(1024)
             if response == b'success':
-                print("success")
                 return out["finished"]
         except Exception as exception:
             print(exception)
             self.Queue.append(out)
             self.close()
             self.initialized = False
-            print("Connection failed due to : " + str(exception))
+            logging.WARNING("Connection failed due to : " + str(exception))
             print("RE-establishing connection")
             new_connection = ConnectionThread(self)
             new_connection.start()
@@ -103,20 +100,31 @@ class UAV_CLIENT(socket):
     
 #Driver code to test the program
 if __name__ == '__main__':
+    logging.basicConfig(filename= 'AIclient.log', filemode= 'w', format= datetime.now().strftime("%d/%m/%Y %H:%M:%S") +' : %(levelname)s : %(message)s')
     connection_string ='/dev/ttyACM0'
-    vehicle = mavutil.mavlink_connection(device=connection_string, baud=57600)
-    cap = cv2.VideoCapture('home/pi/MAH00145.MP4')
+    logging.INFO("Connecting to PIX-HAWK on internal port= " + connection_string + " with baud= 57600")
+    vehicle = mavlink_connection(device= connection_string, baud= 57600)
+    logging.INFO("The PIX-HAWK has been connected successfully")
+    logging.INFO("Connecting to the ground station on ADDRESS=192.168.1.56 and PORT= 5000")
     mysocket = UAV_CLIENT(ADDRESS = "192.168.1.56")
+    logging.INFO("The ground station has been connected successfully")
+    cap = cv2.VideoCapture('home/pi/MAH00145.MP4')
+    logging.INFO("Camera stream has been captured")
     # Get default camera window size
     while True:
         ret, frame = cap.read()
         if not ret:
-            print("no feed")
+            logging.INFO("Stream ended")
             break
-        coordinates = tuple(map(float, str(vehicle.location().split(","))))
+        coordinates = vehicle.location()
+        geolocation = coordinates.split(",")
+        geolocation = map(lambda x : float(x[4:]), coordinates)
+        coordinates = list(coordinates)
         mysocket.sendMission(coordinates, frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        logging.INFO("Frame with location= { " + coordinates + " } has been sent to the ground station")
         sleep(0.1)
     cap.release()
+    logging.INFO("Camera stream has been released")
+    logging.INFO("Ending the mission")
     mysocket.endMission()
+    logging.INFO("Program exited with no errors")
